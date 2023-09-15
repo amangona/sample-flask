076c39e64315635f554cbe75bcfe7864800642fb
Abe Mangona <abrahammangona@gmail.com>
2023-09-15 21:20:56 +0000

    add pusher

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask import render_template
+import pusher
 
 app = Flask(__name__)
 
+# Configure Pusher with your Pusher credentials
+pusher_client = pusher.Pusher(
+    app_id='1638415',
+    key='96b3f7a5a54517450f0d',
+    secret='963323e048f9027c3355',
+    cluster='us2',
+    ssl=True
+)
 
 @app.route("/")
 def hello_world():
